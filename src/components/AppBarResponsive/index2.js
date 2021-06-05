f5985e72758decc59208fea4552a04338cabc2b7
roshan bhatta <roshanbhatta@bottle.com.np>
2021-06-06 00:26:04 +0545

    update sidebar for easy access through database

--- src/components/AppBarResponsive/index2.js
+++ src/components/AppBarResponsive/index2.js
@@ -3,24 +3,70 @@ import {useState,useEffect} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import styled from 'styled-components';
 
-import SwipeableDrawer  from '@material-ui/core/SwipeableDrawer';
 import Drawer from '@material-ui/core/Drawer';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import { ListItemIcon, ListItemText } from '@material-ui/core';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import HomeIcon from '@material-ui/icons/Home';
 import Divider from '@material-ui/core/Divider';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+
+const MenuItemOptions = [
+  {
+    title: 'Home',
+    icon: <HomeIcon/>,
+    path: '#'
+  },
+  {
+    title: 'Activities',
+    icon: null,
+    path: '#',
+    submenu: [
+      {
+        title: 'Football',
+        // icon: <HomeIcon/>,
+        path: '#'
+      },
+      {
+        title: 'Wrestling',
+        // icon: <HomeIcon/>,
+        path: '#'
+      }
+    ]
+  },
+  {
+    title: 'Blogs',
+    icon: <HomeIcon/>,
+    path: '#'
+  },
+  {
+    title: 'About',
+    icon: <HomeIcon/>,
+    path: '#'
+  },
+  {
+    title: null
+  },
+  {
+    title: 'Contact Us',
+    icon: <HomeIcon/>,
+    path:'#'
+  }
+
+];
 
 const AppBarResponsive = ({logo}) => {
 
-    const MenuItemOptions = ['Home','Activities','Blogs','About'];
+    const [subMenuBoolean,setSubMenuBoolen] = useState(Array(MenuItemOptions.length).fill(false));
 
     const [drawerActivate, setDrawerActivate] = useState(false);
     const [drawer, setDrawer] = useState(false);
@@ -56,6 +102,15 @@ const AppBarResponsive = ({logo}) => {
       return ()=>window.removeEventListener('resize',handleResize);
     })
 
+    const popSubMenu = (isPopType,index) =>{
+      if(isPopType){
+        const clone = [...subMenuBoolean];
+        clone[index] = !clone[index];
+        setSubMenuBoolen(clone);
+        // setSubMenuBoolen(...subMenuBoolean[index],!subMenuBoolean[index]);
+      }
+    }
+
     return(
       <div>
         <AppBar position='fixed' color='inherit'>
@@ -65,27 +120,72 @@ const AppBarResponsive = ({logo}) => {
 
                 <div style={{flex:1}}/>
                 {drawerActivate===false && <div>
-                      {/* Menu Options for Larger Displays*/}
-                      {MenuItemOptions.map((item,index)=><CustomMenuButton key={index} id={index}>{item.toUpperCase()}</CustomMenuButton>)}
+                      {
+                        MenuItemOptions.map((item,index)=>{
+                          return(
+                            <>
+                              {
+                                item.title && 
+                                <CustomMenuButton key={index} id={index}>{item.title.toUpperCase()}</CustomMenuButton>
+                              }
+                            </>
+                          );
+                        })
+                      }
                 </div>}
           </Toolbar>
         </AppBar>
 
         <Drawer open={drawer} onClose={() => {setDrawer(false)}}>
-          <List style={{ width: 200}} >
+          <List style={{ width: 250}}>
             {
               MenuItemOptions.map((item,index)=>{
-                return <>
-                      <CListItem key={index} id={index}>
-                        <ListItemIcon><HomeIcon/></ListItemIcon>
-                        <CListItemText primary={
-                          <CMenuTypography>{item.toUpperCase()}</CMenuTypography>
-                        }/>
-                      </CListItem>
-                      </>
+                return (
+                  <>
+                  {
+                  item.title ?
+                    <>
+                        <CListItem button key={index} id={index} onClick={()=>popSubMenu(item.submenu,index)}>
+                          <ListItemIcon>{item.icon}</ListItemIcon>
+                          <CListItemText primary={
+                            <CMenuTypography>{item.title.toUpperCase()}</CMenuTypography>
+                          }/>
+                          {item.submenu && (subMenuBoolean[index] ? <ExpandLess/> : <ExpandMore/>)}
+                        </CListItem>
+                        {
+                          item.submenu &&
+                          <>
+                            <Collapse in={subMenuBoolean[index]} timeout='auto' unmountOnExit>
+                              <Divider/>
+                              <List>
+                                  {
+                                    item.submenu.map((subitem,subindex)=>{
+                                      return (
+                                        <>
+                                          <CListItem button>
+                                          <ListItemIcon>{subitem.icon}</ListItemIcon>
+                                          <CListItemText primary={
+                                            <Typography>{subitem.title.toUpperCase()}</Typography>
+                                          }/>
+                                          </CListItem>
+                                        </>
+                                      );
+                                    })
+                                  }
+                              </List>
+                              <Divider/>
+                            </Collapse>
+                          </>
+                        }
+                    </> :
+                    <>
+                       <Divider/>
+                    </>
+                  }
+                  </>
+                );
               })
             }
-              <Divider/>
           </List>
       </Drawer>
 
