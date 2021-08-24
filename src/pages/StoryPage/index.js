f7c87a9dd475a35afee7805dda2a8d9ac14c2143
Roshan <bhattaroshan@gmail.com>
2021-08-24 22:53:20 +0545

    modifying year slider

--- src/pages/StoryPage/index.js
+++ src/pages/StoryPage/index.js
@@ -11,30 +11,21 @@ import im2 from '../../assets/photo2.jpg';
 import im3 from '../../assets/photo5.jpg';
 
 const timelinemap = [
-    {year:'1'},
-    {year:'2'},
-    {year:'3'},
-    {year:'4'},
-    {year:'5'},
-    {year:'6'},
-    {year:'7'},
-    {year:'8'},
-    {year:'9'},
-    {year:'10'},
-    {year:'11'},
-    {year:'12'},
-    {year:'13'},
-    {year:'14'},
-    {year:'15'},
-    {year:'16'},
-    {year:'17'},
-    {year:'18'},
-    {year:'19'},
-    {year:'19'},
-    {year:'20'},
-    {year:'21'},
-    {year:'22'},
-    {year:'23'},
+    {year:'1990'},
+    {year:'1994'},
+    {year:'1996'},
+    {year:'1998'},
+    {year:'2000'},
+    {year:'2001'},
+    {year:'2004'},
+    {year:'2006'},
+    {year:'2008'},
+    {year:'2010'},
+    {year:'2011'},
+    {year:'2012'},
+    {year:'2013'},
+    {year:'2014'},
+    {year:'2015'},
 ]
 
 const story = [
@@ -75,36 +66,51 @@ const story = [
 const StoryPage = () =>{
     const divRef = useRef();
     const yearRef = useRef();
-    const [dragDisable,setDragDisable] = useState(false);
     const [position,setPosition] = useState({x:0,y:0});
     const [moveToOrigin,setMoveToOrigin] = useState(false);
 
+    const [hightlightYear,setHighlightYear] = useState(1);
+
     const handleYearClick = (index) =>{
-        yearRef.current.backgroundColor = 'red';
+        setHighlightYear(index+1);
     }
 
     const handleStop = ()=>{
         const divLeft = divRef.current.getBoundingClientRect().left;
+        const divRight = divRef.current.getBoundingClientRect().right;
         const yearLeft =yearRef.current.getBoundingClientRect().left;
-        if(yearLeft>divLeft){
+        const yearRight =yearRef.current.getBoundingClientRect().right;
+        if(yearLeft>divLeft || yearRight<divRight){
             setMoveToOrigin(true);
-        }else{
         }
+
+        
     }
 
+
     const handleDrag = (e,data) =>{
-        console.log('drag detected');
         const divLeft = divRef.current.getBoundingClientRect().left;
+        const divRight = divRef.current.getBoundingClientRect().right;
         const yearLeft =yearRef.current.getBoundingClientRect().left;
-        // console.log(res);
+        const yearRight =yearRef.current.getBoundingClientRect().right;
+
+        const divWidth = divRight-divLeft;
+        const yearWidth = yearRight-yearLeft;
+        let rem = 0;
+        if(divWidth>yearWidth){
+            rem = divWidth-yearWidth;
+        }
+
         if(yearLeft>divLeft){
             setPosition({x:0,y:0});
-        }else{
+        }else if(yearRight<divRight){
+            setPosition({x:divWidth-yearWidth-rem,y:0})
+        }
+        else{
             setPosition({x:data.x,y:0});
             setMoveToOrigin(false);
         }
-        console.log('div left = ',divLeft);
-        console.log('year left = ',yearLeft);
+
     }
 
     return (
@@ -114,9 +120,11 @@ const StoryPage = () =>{
                     <Sdiv className='story-timeline-container' ref={yearRef} moveToOrigin={moveToOrigin}>
                             {
                                 timelinemap.map((elem,index)=>{
-                                    return <span key={index} id={index} onClick={()=>handleYearClick(index)}>
+                                    return <Sspan key={index} id={index} highlight={hightlightYear}
+                                            onClick={()=>handleYearClick(index)}  
+                                           >
                                                 {elem.year}
-                                        </span>
+                                        </Sspan>
                                 })
                             }
 
@@ -134,4 +142,10 @@ export default StoryPage;
 
 const Sdiv = styled.div`
     transition: ${props=>props.moveToOrigin?'all 0.5s ease':'none'};
+`;
+
+const Sspan = styled.span`
+    &:nth-child(${props=>props.highlight}){
+        border-bottom: 3px solid black;
+    }
 `;
